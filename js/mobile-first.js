d87da6cd25c4259e95aa4068f29ab9dd2e983bf2
Christopher Bilger <christopherbilg@gmail.com>
2019-11-22 12:22:43 -0500

    Renamed file and changed JS function onclick of menu

---
+++ js/mobile-first.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 8*/
+
+(function(){
+  "use strict";
+
+  var menu = document.getElementById("menu");
+  
+  menu.onclick = function() {
+    window.location.href = "./materialize.html";
+  };
+})();
